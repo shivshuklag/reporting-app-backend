109ffdea6e0d93252e610e5ae9838eebe6e98ac6
Shiv <shiv@ip-192-168-29-210.ap-south-1.compute.internal>
2025-02-03 17:53:25 +0530

    Added validation middleware to enable it

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({ origin: 'http://localhost:3000', credentials: true });
 
